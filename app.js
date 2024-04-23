9e03e888fd7580e6c20d30d4aa7974e0c13b8cc0
João Vítor <jaovit81@gmail.com>
2024-04-23 20:15:21 -0300

    criação do servidor

---
+++ app.js
@@ -0,0 +1,35 @@
+const { exec } = require('child_process');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+app.post('/process-login', (req, res) => {
+    const { cpf, senha, pesquisa } = req.body;
+
+    console.log('Dados recebidos no lado do servidor:', cpf, senha, pesquisa);
+
+    exec(`node automacaoCopasa.js "${cpf}" "${senha}" "${pesquisa}"`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Erro na execução do script: ${error.message}`);
+            res.status(500).send('Erro na execução do script');
+            return;
+        }
+        if (stderr) {
+            console.error(`Erro no script: ${stderr}`);
+            res.status(500).send('Erro no script');
+            return;
+        }
+        console.log(`Resultado do script: ${stdout}`);
+        res.send('Faturas baixadas com sucesso!');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
