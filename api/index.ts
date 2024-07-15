d5b83bb6e3d315ce0b32a7b966c71d6b3799edd6
João Vítor <jaovit81@gmail.com>
2024-07-15 20:43:07 -0300

    alteração do server

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 const { exec } = require('child_process');
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
@@ -9,12 +10,14 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/login.html');
 });
 
+const scriptPath = path.join(__dirname, '..', 'automacaoCopasa.js');
+
 app.post('/process-login', (req, res) => {
     const { cpf, senha, pesquisa } = req.body;
 
     console.log('Dados recebidos no lado do servidor:', cpf, senha, pesquisa);
 
-    exec(`node automacaoCopasa.js "${cpf}" "${senha}" "${pesquisa}"`, (error, stdout, stderr) => {
+    exec(`node "${scriptPath}" "${cpf}" "${senha}" "${pesquisa}"`, (error, stdout, stderr) => {
         if (error) {
             console.error(`Erro na execução do script: ${error.message}`);
             res.status(500).send('Erro na execução do script');
